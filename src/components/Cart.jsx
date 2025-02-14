88800b223be878e78283a6a555f98331e9958834
rakib-hasan-78 <rakibwebdev78@gmail.com>
2025-02-15 05:43:37 +0600

    scroll sticky for cart done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,14 +2,36 @@ import React, { useEffect, useState } from 'react';
 
 const Cart = () => {
 
+  const [isScroll, setIsScroll] = useState(false);
+  const [cartPosition, setCartPosition] = useState(0);
+
+  useEffect(()=>{
+    const cartElement = document.getElementById('cart');
+    setCartPosition(cartElement.offsetTop);
+
+    const cartScrollHandler = () => {
+      const  scrollPosition = window.scrollY;
+
+      // checking differences between scroll & offset Position ===>
+        scrollPosition>=cartPosition ? setIsScroll(true) : setIsScroll(false);
+    }
+
+    window.addEventListener('scroll', cartScrollHandler);
+    return ()=> {
+      window.removeEventListener('scroll', cartScrollHandler);
+    }
+
+  },[cartPosition])
+
+
 
   return (
     <section
       id="cart"
-      className={`w-75 border mx-auto`}
-      style={{ transition: 'top 0.3s ease' }}
+      className={`w-75 border mx-auto fixed-top-transition ${isScroll ? 'fixed-top':''}`}
     >
       {/* Cart content */}
+      
     </section>
   );
 };
